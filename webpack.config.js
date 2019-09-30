327e707506a00675d9784a78974eacc7d1fe0fa4
Taras Kozlovskyi <tarasroz@yahoo.com>
2019-09-30 14:41:52 +0300

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module:{
+        rules:[{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'jshint-loader'
+        }]
+    }
+};
